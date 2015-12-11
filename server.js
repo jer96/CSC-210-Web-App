64ca162ed14b845a73d565fb2ee1598b3d806ae9
jer96 <jbill3@u.rochester.edu>
2015-12-11 13:23:24 -0500

    Final CSC 210 App
    UR Market App CSC 210

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-// Lectures 11 and 12 - CSC 210 Fall 2015
-// Philip Guo
-
-// This is the backend for the Fakebook web app, which demonstrates CRUD
-// with Ajax using a REST API. (static_files/fakebook.html is the frontend)
+// This is the backend for URMarket
+// with Ajax 
 
 // Prerequisites - first run:
 //   npm install express
 //   npm install body-parser
 //   npm install mysql
+//   npm install nodemailer
 //
 // then run:
 //   node server.js
 //
-// and the frontend can be viewed at http://localhost:3000/fakebook.html
+// and the frontend can be viewed at http://localhost:3000/login.html
 
 var express = require('express');
 var app = express();
@@ -22,18 +20,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-// put all of your static files (e.g., HTML, CSS, JS, JPG) in the static_files/
+// put all of  static files (e.g., HTML, CSS, JS, JPG) in the static_files/
 // sub-directory, and the server will serve them from there. e.g.,:
-//
-// http://localhost:3000/fakebook.html
-// http://localhost:3000/cat.jpg
-//
-// will send the file static_files/cat.jpg to the user's Web browser
 app.use(express.static('static_files'));
 
 var mysql = require('mysql');
 
-// First you need to create a connection to the db
+// Creating connection to database
 var pool = mysql.createPool({
   host: 'localhost',
   port:'3306',
@@ -41,17 +34,45 @@ var pool = mysql.createPool({
   password: '9696',
   database: 'users'
 });
+//Node mailer function which is used to send the seller an email from the potential buyer
+var emailer = require('nodemailer');
+var transporter = emailer.createTransport({
+  service: 'Gmail',
+  auth:{
+    user: 'urmarket1234@gmail.com',
+    pass: '*O-line96'
+  }
+});
 
 
+//Get request for the send email function
+app.get('/send', function(req , res){
+
+  var mail = {
+  to: req.query.to,
+  subject: 'UR Market Buyer Request ',
+  text: req.query.text,
 
 
+  }
+  transporter.sendMail(mail,function(error, response){
+    if(error){
+      console.log(error);
+      res.send("error");
 
+    }
+    else{
+      console.log("Message was sent!");
+      res.end("sent");
+    }
+    
 
+  });
+  
+
+});
 
 // CREATE a new user
-//
-// To test with curl, run:
-//   curl -X POST --data "name=Carol&job=scientist&pet=dog.jpg" http://localhost:3000/users
 app.post('/users', function (req, res) {
   var postBody = req.body;
   var myName = postBody.username;
@@ -84,6 +105,7 @@ con.release();
 
 });
 
+//Create new listing
 app.post('/listing', function (req, res) {
   var postBody = req.body;
   var myName = postBody.itemName;
@@ -111,44 +133,57 @@ app.post('/listing', function (req, res) {
  
 });
 
-
-
-
-
-
-
 // READ profile data for a user
-//
-// To test with curl, run:
-//   curl -X GET http://localhost:3000/users/Philip
-//   curl -X GET http://localhost:3000/users/Jane
 
 app.get('/users/*', function (req, res) {
   var nameToLookup = req.params[0];
-  console.log(nameToLookup) // this matches the '*' part of '/users/*' above
-  // try to look up in fakeDatabase
   pool.getConnection(function(err, con){
   con.query('SELECT * FROM user',function(err,rows){
+  if(err){
+    res.send(err);
+  console.log(err);
+  console.log("error herr");
+}
+
+
+  for (var i = 0; i < rows.length; i++) {
+   
+    if (rows[i].username == nameToLookup) {
+      console.log("inside  loop");
+      res.send(rows[i]);
+      console.log(rows[i]);
+      return; // return early!
+    }
+  }
+  res.send("error");
+});
+con.release(); 
+});
+});
+
+//Method to find the user who is the seller of a given item
+app.get('/seller/*', function (req, res) {
+  var nameToLookup = req.params[0];
+  pool.getConnection(function(err, con){
+  con.query('SELECT * FROM user where username =?', nameToLookup , function(err,rows){
   if(err){
   console.log(err);
   return;
 }
   for (var i = 0; i < rows.length; i++) {
    
-    if (rows[i].username == nameToLookup) {
+    
       res.send(rows[i]);
-      console.log('read info success');
-      return; // return early!
-    }
-    else{
-      console.log('no user in db');
-    }  
+    
+     
   }
 });
 con.release(); 
 });
 });
 
+
+//Find listings specific to a user
 app.get('/listings/*', function (req, res) {
   var nameToLookup = req.params[0];
   console.log(nameToLookup+ " server communication"); // this matches the '*' part of '/users/*' above
@@ -163,10 +198,99 @@ app.get('/listings/*', function (req, res) {
   
    
   for(var i = 0; i < rows.length ; i++){
- res.send(rows[i]);
+
  console.log(rows[i]);
+
 console.log('read info success');
 }
+ res.send(rows);   
+  
+});
+con.release(); 
+});
+});
+
+//Used to pull a certain users listings from the database
+app.get('/edit/*', function (req, res) {
+  var nameToLookup = req.params[0];
+  console.log(nameToLookup+ " server communication"); 
+  pool.getConnection(function(err, con){
+
+  con.query('SELECT * FROM listing  where itemName = ? ',nameToLookup , function(err,rows){
+  if(err){
+    console.log(err);
+    return;
+  }
+ for (var i = 0; i < rows.length; i++) {
+   
+    
+      res.send(rows[i]);
+      console.log(rows[i]);
+  }  
+  
+});
+con.release(); 
+});
+});
+
+//Retrieve listings with a specific id which is the primmary key
+app.get('/view/*', function (req, res) {
+  var nameToLookup = req.params[0];
+  console.log(nameToLookup+ " server communication"); 
+  pool.getConnection(function(err, con){
+
+  con.query('SELECT * FROM listing  where id = ? ',nameToLookup , function(err,rows){
+  if(err){
+    console.log(err);
+    return;
+  }
+ for (var i = 0; i < rows.length; i++) {
+   
+    
+      res.send(rows[i]);
+      console.log(rows[i]);
+  }  
+  
+});
+con.release(); 
+});
+});
+
+//Read all the listings from the database
+app.get('/show', function (req, res) {
+  
+  pool.getConnection(function(err, con){
+
+  con.query('SELECT * FROM listing ', function(err,rows){
+  if(err){
+    console.log(err);
+    return;
+  }
+      res.send(rows);
+});
+con.release(); 
+});
+});
+
+//Retrieve listings of a certain category
+app.get('/show/*', function (req, res) {
+  var cat = req.params[0];
+  
+  pool.getConnection(function(err, con){
+
+  con.query('SELECT * FROM listing where category =  ?',cat , function(err,rows){
+  if(err){
+    console.log(err);
+    return;
+  }
+
+   
+    
+      res.send(rows);
+     
+   
+    
+
     
   
 });
@@ -174,33 +298,10 @@ con.release();
 });
 });
 
-
-
-
-// READ a list of all usernames (note that there's no '*' at the end)
-//
-// To test with curl, run:
-//   curl -X GET http://localhost:3000/users
-app.get('/users', function (req, res) {
-  var allUsernames = [];
-
-  for (var i = 0; i < fakeDatabase.length; i++) {
- var e = fakeDatabase[i];
-  allUsernames.push(e.username); // just record names
- }
-
- res.send(allUsernames);
-});
-
-
 // UPDATE a user's profile with the data given in POST
-//
-// To test with curl, run:
-//   curl -X PUT --data "job=bear_wrangler&pet=bear.jpg" http://localhost:3000/users/Philip
 app.put('/users/*', function (req, res) {
   var nameToLookup = req.params[0]; 
-  var postBody = req.body;// this matches the '*' part of '/users/*' above
-  // try to look up in fakeDatabase
+  var postBody = req.body;
   console.log(nameToLookup);
   pool.getConnection(function(err,con){
  con.query('UPDATE user SET username = ? , fname = ? , lname = ? , phone = ? , year = ? , icon = ? WHERE username=  ?' ,[postBody.username , postBody.fname , postBody.lname , postBody.phone , postBody.year , postBody.icon , nameToLookup] , function(err,result){
@@ -218,16 +319,32 @@ console.log('Changed ' + result.changedRows + 'rows');
 
 });
 
+//Update listings
+app.put('/edit/*', function (req, res) {
+  var nameToLookup = req.params[0]; 
+  var postBody = req.body;
+  console.log(nameToLookup);
+  pool.getConnection(function(err,con){
+ con.query('UPDATE listing SET itemName = ? , itemCondition = ? , itemDescription = ? , itemPrice = ? ,category = ?, icon = ? WHERE username = ? AND itemName = ?' ,[postBody.itemName, postBody.itemCondition, postBody.itemDescription, postBody.itemPrice ,postBody.category, postBody.icon ,nameToLookup,postBody.itemName] , function(err,result){
+if(err){
+  console.log(err);
+  res.send('ERROR');
+  
+}
+
+res.send('OK');
+console.log('Changed ' + result.changedRows + 'rows');
+
+
+ } );
+ con.release();
+});
+
+});
 
 // DELETE a user
-//
-// To test with curl, run:
-//   curl -X DELETE http://localhost:3000/users/Philip
-//   curl -X DELETE http://localhost:3000/users/Jane
 app.delete('/users/*', function (req, res) {
   var nameToLookup = req.params[0];
-  // this matches the '*' part of '/users/*' above
-  // try to look up in fakeDatabase
   pool.getConnection(function(err,con){
   con.query('DELETE FROM user WHERE username = ?' , nameToLookup , function(err, result){
     if(err){
@@ -240,9 +357,27 @@ app.delete('/users/*', function (req, res) {
     
   });
 });
-  // nobody in the database matches nameToLookup
+
 });
 
+//Delete a listing
+app.delete('/listings/*', function (req, res) {
+  var nameToLookup = req.params[0];
+  console.log(nameToLookup);
+  pool.getConnection(function(err,con){
+  con.query('DELETE FROM listing WHERE itemName = ?' , nameToLookup , function(err, result){
+    if(err){
+      console.log(err);
+      res.send('ERROR');
+      return;
+    }
+    res.send('OK');
+    console.log('acccount deleted');
+    
+  });
+});
+
+});
 
 // start the server on http://localhost:3000/
 var server = app.listen(3000, function () {
